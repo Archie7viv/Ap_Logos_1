ab44adad44bc4a6c96a9c9ab02287747bfb9edcf
Archie7viv <popoff7viv@gmail.com>
2016-11-30 05:17:25 -0500

    Third application

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 2 числа з клавіатури і вибираємо для них дію за числом з клавіатури
+            /*
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vvedit chuslo vid 1 do 4: \n 1:+ \n 2:- \n 3:* \n 4:/");
+            int x = int.Parse(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine(n1 + n2);
+                    break;
+                case 2:
+                    Console.WriteLine(n1 - n2);
+                    break;
+                case 3:
+                    Console.WriteLine(n1 * n2);
+                    break;
+                case 4:
+                    Console.WriteLine(n1 / n2);
+                    break;
+            }
+            */
+
+            // ввести рік з клавіатури і визначити чи він високосний
+            /*
+            Console.WriteLine("Vvedit rik");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0 && year % 400 !=0)
+            {
+                Console.WriteLine("Rik visokosniy");
+            }
+            else {
+                Console.WriteLine("Rik ne visokosniy");
+            }
+            */
+
+            // ввести з клавіатури 3 сторони трикутника і порахувати чи такий трикутник може існувати
+
+            /*
+            Console.WriteLine("Vvedit 3 storonu trukytnuka");
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            if (x1 + x2 > x3 && x2 + x3 > x1 && x1 + x3 > x2)
+            {
+                Console.WriteLine("existed");
+            }
+            else {
+                Console.WriteLine("Not existed");
+            }
+            */
+            /*
+            int num;
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+            }
+
+            Console.WriteLine(sum);
+            */
+            /*
+            int num = 1;
+            int sum = 0;
+            while (num != 0) {
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                Console.WriteLine("sum = " + sum);
+            }
+            */
+
+            // вводимо 5 чисел і рахуємо їх середнє арифметичне
+            /*
+            Console.WriteLine("Vvedit 5 chusel");
+            int c;
+            int sum = 0;
+            double a;
+            for (int i = 0; i < 5; i++) {
+                c = int.Parse(Console.ReadLine());
+                sum = sum + c;
+            }
+            Console.WriteLine(a = sum / 5);
+            */
+
+            // середнє арифметичне для багатьох чисел поки не введеться 0, виводимо середнє і кількість циклів
+            /*
+            Console.WriteLine("Enter any numbers");
+            int c;
+            int sum = 0;
+            double a;
+            int i=0;
+            do{
+                c = int.Parse(Console.ReadLine());
+                sum = sum + c;
+                i++;
+            } while (c != 0);
+            a = sum / i;
+            Console.WriteLine("Average is " + a);
+            Console.WriteLine("Number of loops is " + i);
+            */
+
+
+            // ??????????????????????
+            /*
+            int num, i, j, k;
+            Console.Write("enter the level:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                for (j = 1; j < num - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+             }
+             */
+
+
+            // кількість парних чисел у натуральному числі
+            /*
+            int n = 123456648;
+            int count = 0;
+            while (n > 0)
+            {
+                if ((n % 10) % 2 == 0) count++;
+                n /= 10;
+            }
+            Console.WriteLine(count);
+            */
+
+
+            //Найти среднее арифметическое всех целых чисел от а до b (значения а и b вводятся с клавиатуры; ) 
+            /*
+            double a, b;
+            double sum = 0;
+            double result = 0;
+            Console.Write("Введите a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = double.Parse(Console.ReadLine());
+
+            for (double i = a; i <= b; i++)
+
+            {
+                sum += i;
+                result += i / ((b - a) + 1);
+            }
+            
+            Console.WriteLine("Cумма чисел от {0} до {1} ровна {2}, среднее арифметическое равно {3}: ", a, b, sum, result);
+            */
+
+            //3.) Начав тренировки, лыжник в первый день пробежал 10 км. Каждый следующий день он увеличивал пробeг на 10% от пробега предыдущего дня. Определить в какой день суммарный пробег за все дни превысит 100 км. 
+            
+            /*
+            double sum = 10;
+            int day = 1;
+            double distPerDay = 10;
+
+            while (sum < 100)
+            {
+                double inc = distPerDay / 10.0;
+                distPerDay = distPerDay + inc;
+                sum += distPerDay;
+                day++;
+            }
+            Console.WriteLine(day);
+            */
+
+            /* 3b)
+            int day = 1;
+            double dayDist = 10;
+
+            for (double sum = 10; sum < 100; day++)
+            {
+                double inc = dayDist / 10.0;
+                dayDist += inc;
+                sum += dayDist;
+            }
+            //Console.WriteLine("На " + day + "відстань перевищить 100км");
+            Console.WriteLine(day);
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int k = int.Parse(Console.ReadLine());
+            string result = k > 10 ? "k is greater than 10" : "k is " + k;  // тернарний оператор: якщо true - то 1й варіантЮ якщо false - другий
+            Console.WriteLine(result);
+            */
+
+            // ТРИКУТНИК
+            /*
+           int numberoflines = 4;
+           for (int i = 0; i < numberoflines; i++)
+           {
+               for (int spaces = 0; spaces < numberoflines -i - 1; spaces ++)
+               {
+               Console.Write(" ");
+               }
+
+               for (int arrows = 0; arrows < i * 2 + 1; arrows++) 
+               {
+                   Console.Write("^");
+               }
+               Console.WriteLine();
+           }
+           */
+
+
+            //МАСИВ 1
+            /*
+            int [] students = new int [6];
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine("Student #{0} has {1} points", i, students[i]);
+            }
+           */
+
+            // вивести всі додатні числа з масиву та їх кількість
+            /*
+            int[] newArr = { 3, -3, -5, 5, 6, 7 };
+            int amount = 0;
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (newArr[i] > 0)
+                {
+                    amount += 1;
+                    Console.WriteLine(newArr[i]);
+                    
+                }
+            }
+            Console.WriteLine("Amount of positive values is " + amount);
+            */
+
+            // ввести 10 чисел в масив та вивести найбільше значення з них
+
+            /*
+            int[] myArr = new int[10];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                myArr[i] = int.Parse(Console.ReadLine());
+            }
+            int max = int.MinValue;  // або int max = myArr[0] - починаємо з нульового елементу
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                if (myArr[i] > max)
+                    max = myArr[i];
+            }
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                Console.Write(" " + myArr[i]);
+            }
+            Console.WriteLine("\n Max value is " + max);
+            */
+
+            //є масив {3, 5, 6, 0, 1, 2, 1, 0, 6, 4} - вивести всі числа між нулями - ППЦ!
+
+            /*
+            int [] nums = { 5, 7, 3, 9, 0, 5, 1, 2, 0, 6 };
+            int firstZeroIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    firstZeroIndex = i;
+                    break;
+                }
+            }
+            for (int i = firstZeroIndex + 1; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                    Console.WriteLine(nums[i]);
+                else
+                    break;                
+            }
+            */
+
+
+            //вводимо числа поки не введеться 0
+            /*
+            int num = 1;
+            while (num != 0)
+            {
+                Console.WriteLine("Vvedit chuslo");
+                num = int.Parse(Console.ReadLine());
+            }
+            */
+            /*
+            int num = 1;
+            while (true)
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num == 0)
+                {
+                    break;
+                }
+            }
+            */
+            /*
+            int[] nums = { -3, 2, 4, 3, 2, 1 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0) //виводимо перший елемент, який більше 0
+                {
+                    Console.WriteLine(nums[i]);
+                    break;
+                }
+            }
+            int sum = 0; ;
+            foreach (int elem in nums)
+            {
+                sum += elem; //рахуємо суму чисел в масиві
+                Console.Write(elem + " ");
+            }
+            Console.WriteLine("sum = " + sum);
+            */
+
+
+
+            //!!!багатовимірний масив
+
+
+
+
+            // сума елементів двовимірного масиву
+
+            /*
+            int[,] ar1 = new int[3, 4];
+            int sum = 0;
+            for (int i = 0; i < 3; i++)     // заповнимо масив
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    ar1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(ar1[i, j] + "\t");  //виводимо масив
+                    sum += ar1[i, j];
+                }
+                Console.WriteLine(); // виводимо масив
+            }
+            Console.WriteLine("Sum is {0}", sum); //виводимо суму
+             */
+
+            // східчатий масив
+            /*
+            int[][] ar2 = new int[3][];
+            for (int i = 0; i < 3; i++)
+            {
+                ar2[i] = new int[i+1];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < ar2[i].Length; j++)
+                {
+                    ar2[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int max = int.MinValue; // визначаємо найбільше значення в масиві
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < ar2[i].Length; j++)
+                {
+                    Console.Write(ar2[i][j] + "\t"); //виводимо весь масив на екран
+                    if (ar2[i][j] > max)
+                        max = ar2[i][j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Max is {0}", max);
+            */
+
+
+            // порахувати суму всіх елементів, які знаходяться в непарних рядках
+            /*
+            int[,] ar4 = new int[4, 3];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    ar4[i,j] = int.Parse(Console.ReadLine());
+                }
+  
+            }
+            for (int i = 0; i < 4; i++) // виводимо масив
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(ar4[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0; // рахуємо суму в непарних рядках
+
+            for (int row = 0; row < 4; row++)
+            {
+                if (row % 2 != 0)
+                {
+                    for (int column = 0; column < 3; column++)
+                    {
+                        sum += ar4[row, column];
+                    }
+                }
+               
+            }
+            Console.WriteLine(sum);
+            */
+
+
+            // Arrays - HomeTask #1 (масив по діагоналі, друга діагональ, сума чисел під головною діагоналлю)
+            /*
+            Console.WriteLine("Enter matrix size");
+            int size = int.Parse(Console.ReadLine());
+            int[,] arr = new int[size, size];
+            Console.WriteLine("Enter elements");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            
+            //arr = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i < size; i++)  //виводимо масив
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int i = 0; i < size; i++)  // основна діагональ
+            {
+                sum1 += arr[i, i];
+            }
+
+            int newSize = 1;
+            for (int i = 0; i < size - 1; i++, newSize++) // друга діагональ ??
+            {
+                for (int j = newSize; j <= newSize; j++)
+                {
+                    sum2 += arr[i, j];
+                }
+            }
+            Console.WriteLine("Sum of main diagonal is {0} \n Sum of 2nd diagonal is {1}", sum1, sum2);
+
+            int sum3 = 0;
+            for (int i = 1; i < size; i++) //сума елементів під діагоналлю
+            {
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.WriteLine(arr[i, j]);
+                        sum3 += arr[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Sum3 is " + sum3);
+            Console.ReadLine();
+            */
+
+            //Arrays - Hometask 2 - слово з клавіантури та перевірити чи воно є паліндромом
+            /*
+            Console.WriteLine("Enter any word");
+            string word = Console.ReadLine();
+            char[] charArray = word.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                Console.WriteLine(charArray[i]);
+            }
+            int reverse = charArray.Length -1;
+            int condition;
+            for (int i = 0; i < reverse; i++, reverse--)
+            {
+                if (charArray[i] == charArray[reverse])
+                {
+                    Console.WriteLine(word + " is a palindrom");
+                }
+                else
+                {
+                    Console.WriteLine(word + " is not a palindrom!!!");
+                }
+            */
+
+
+            }
+        }
+    }
+}
