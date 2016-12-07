e49456bad3f3fba926d7715f8d5cbe91cb653a08
Archie7viv <popoff7viv@gmail.com>
2016-12-07 09:33:16 -0500

    Classes - Properties

--- ConsoleApplication - Classes/ConsoleApplication - Classes/Computer.cs
+++ ConsoleApplication - Classes/ConsoleApplication - Classes/Computer.cs
@@ -6,6 +6,166 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication___Classes
 {
+    class CustomFloat
+    {
+        private int firstPart;
+        private float secondPart;
+
+        //const float PI = 3.14f;
+
+        //readonly float numOfNumbers;
+
+        public CustomFloat(float number)
+        {
+            firstPart = (int)number;
+            secondPart = number - firstPart;
+        }
+        public int FirstPart
+        {
+            set
+            {
+                firstPart = value;
+            }
+        }
+
+        public float SecondPart
+        {
+            set
+            {
+                if (Math.Abs(value) < 1f)
+                    secondPart = value;
+                else
+                    secondPart = 0f;
+            }
+        }
+        /*public void Print()
+        {
+            Console.WriteLine(firstPart + secondPart);
+        }*/
+
+
+        public int ReturnInt()
+        {
+            return firstPart;
+        }
+        public float ReturnFloat()
+        {
+            return secondPart;
+        }
+    }
+}
+
+
+
+    /*
+    class Employee
+    {
+        private string name;
+        private int id;
+        private int salary;
+
+        
+        public string Speciality { get; set; } //коротка властивість - змінна створюється автоматично при компіляції
+
+        public string Name //property (властивість, надбудова)
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        
+            public Employee(string newName, int newId, int newSalary)
+        {
+            name = newName;
+            id = newId;
+            salary = newSalary;
+        }
+
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public int Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value - this.salary >= 500)
+                    this.salary += 500;
+                else if (this.salary - value >= 500)
+                    this.salary -= 500;
+                else
+                    this.salary = value;
+            }
+        }
+        */
+
+    /*
+    public int GetSalary() //оскільки поле приватне - ми його не бачимо і цей метод допомагає вивести його в Main
+    {
+        return salary;
+    }
+
+    public void SetSalary(int Salary)  //хочемо збільшити на 2000, але збільшуємо усього на 500
+    {
+        if (salary - this.salary >= 500)
+            this.salary += 500;
+        else if (this.salary - salary >= 500)
+            this.salary -= 500;
+        else
+            this.salary = salary;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine("Employee Info: {0}, {1}, {2}",name, id, salary);
+    }
+ */
+ 
+    /*
+    class Mouse
+    {
+        public int numOfKeys;
+        public int dpi;
+        public string model;
+
+        public Mouse(string mModel = "", int mNumOfKeys = 2, int mDPI = 800)
+        {
+                model = mModel;
+                numOfKeys = mNumOfKeys;
+                SetDpi(mDPI);
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Mouse Info: {0}, {1}, {2}", model, numOfKeys, dpi);
+        }
+
+        public int GetDpi()
+        {
+            return dpi;
+        }
+
+        public void SetDpi(int dpi)
+        {
+            this.dpi = dpi - (dpi % 400);
+        }
+    }
+    */
+
+    /*
     class Computer
     {
         public string Model;
@@ -41,10 +201,5 @@ namespace ConsoleApplication___Classes
             else
                 Console.WriteLine("Your Computer is Off");
         }
+    }*/
 
-
-
-
-
-    }
-}
--- ConsoleApplication - Classes/ConsoleApplication - Classes/Program.cs
+++ ConsoleApplication - Classes/ConsoleApplication - Classes/Program.cs
@@ -19,13 +19,61 @@ namespace ConsoleApplication___Classes
             */
 
             //HomeTask1 - Constructors
-            Computer computer = new Computer("Dell", 320, 2048, true);
+
+            /*Computer computer = new Computer("Dell", 320, 2048, true);
             computer.CompInfo();
             Console.ReadLine();
             computer.IsOnMethod();
-                        
-        }
+            */
 
+
+
+            //Encapsulation
+            /*
+            Mouse m1 = new Mouse("Logitech", 5, 1999);
+
+            m1.model = "ASUS";
+            Console.WriteLine("New mouse: {0} {1} {2}", m1.model, m1.numOfKeys, m1.dpi);
+
+            m1.PrintInfo();
+            m1.SetDpi(1999); // виводиться найближче значення яке ділиться на 400
+            Console.WriteLine(m1.GetDpi());
+            */
+
+            //Task - Employee
+            /*
+            Employee Emp = new Employee("John", 321, 2000);
+            //Console.WriteLine("Початкова зарплата = {0}", Emp.GetSalary()); // вертаємо початкову зарплату
+            //Emp.SetSalary(10); //хочемо збільшити на 2000, але збільшуємо усього на 500
+            Emp.Salary = 3000;
+            Console.WriteLine(Emp.Salary);
+            Emp.Salary = 4000;
+            Console.WriteLine(Emp.Salary);
+            Emp.Salary = 5000;
+            Console.WriteLine(Emp.Salary);
+            Emp.PrintInfo();
+            Console.WriteLine(Emp.Name);
+            Emp.Name = "Gregor";
+            Console.WriteLine(Emp.ID);
+
+            //Console.WriteLine(Emp.GetSalary());  //оскільки поле Salary приватне - ми його не бачимо і цей метод допомагає вивести його в Main
+            */
+
+            string value = Console.ReadLine();
+            float x = (float)Convert.ToDouble(value);
+            CustomFloat Num = new CustomFloat(x);
+
+            Console.WriteLine(Num.ReturnInt());
+            Console.WriteLine(Num.ReturnFloat());
+            Console.ReadLine();
+
+            /*
+            Num.FirstPart = 5;
+            Num.SecondPart = 9.4f;
+            Num.Print();
+            */
+
+        }
     }
         
     
